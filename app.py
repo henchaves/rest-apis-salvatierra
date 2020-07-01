3328462880d2de6d130b828658e0984f3f8fe56b
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-01 18:10:43 -0300

    Fixed bugs

--- app.py
+++ app.py
@@ -44,14 +44,14 @@ def get_stores():
     return jsonify({"stores": stores})
 
 #POST /store/<string:name>/item {name:, price:}
-@app.route("/sotre/<string:name>/item", methods=["POST"])
+@app.route("/store/<string:name>/item", methods=["POST"])
 def create_item_in_store(name):
     request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
             new_item = {
                 "name": request_data["name"],
-                "pice": request_data["price"]
+                "price": request_data["price"]
             }
             store["items"].append(new_item)
             return jsonify(new_item)
