641342cea0c814d791f227bb983d2b02f796c58b
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-05 19:41:45 -0300

    Writing Item resources to a db

--- section5/code/create_tables.py
+++ section5/code/create_tables.py
@@ -9,7 +9,5 @@ cursor.execute(create_table_query)
 create_table_query = "CREATE TABLE IF NOT EXISTS items (name TEXT, price REAL)"
 cursor.execute(create_table_query)
 
-cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
-
 connection.commit()
 connection.close()
