733e50daebf767c0d54c87c349980129d5c973a1
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-05 19:17:30 -0300

    Retrieving Item resources from a db

--- section5/code/create_tables.py
+++ section5/code/create_tables.py
@@ -6,5 +6,10 @@ cursor = connection.cursor()
 create_table_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
 cursor.execute(create_table_query)
 
+create_table_query = "CREATE TABLE IF NOT EXISTS items (name TEXT, price REAL)"
+cursor.execute(create_table_query)
+
+cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
+
 connection.commit()
 connection.close()
