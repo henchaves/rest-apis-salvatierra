873bde0228bcb281cbb41434ae10c32ba6075a8b
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-06 19:27:38 -0300

    Created User and Item models

---
+++ section6/code/resources/item.py
@@ -0,0 +1,77 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("price", type=float, required=True, help="This field cannot be left blank!")
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {"message": "Item not found."}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {"message": f"An item with name '{name}' already exists."}, 400
+        
+        data = Item.parser.parse_args()
+        item = ItemModel(name, data["price"])
+
+        try:
+            item.insert()
+        except:
+            return {"message": "An error ocurred while inserting the item."}, 500
+
+        return item.json(), 201
+    
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        
+        return {"message": "Item deleted."}
+    
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, data["price"])
+
+        if item is None:
+            try:
+                updated_item.insert()
+                return updated_item.json(), 201
+            except:
+                return {"message": "An error ocurred while inserting the item."}, 500
+        else:
+            try:
+                updated_item.update()
+                return updated_item.json(), 200
+            except:
+                return {"message": "An error ocurred while updating the item."}, 500
+
+
+class ItemList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({"name": row[0], "price": row[1]})
+
+        connection.close()
+
+        return {"items": items}
