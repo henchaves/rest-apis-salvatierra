50f01304f0cde14ce00b27b65d3eb585a5eaaa0c
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-15 17:23:08 -0300

    Minor changes to ItemModel

--- adv-section1/resources/item.py
+++ adv-section1/resources/item.py
@@ -2,9 +2,6 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
     jwt_required, 
     fresh_jwt_required, 
-    get_jwt_claims, 
-    jwt_optional, 
-    get_jwt_identity
 )
 from models.item import ItemModel
 
@@ -24,7 +21,6 @@ class Item(Resource):
         help="Every item needs a store id.",
     )
 
-    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -55,9 +51,6 @@ class Item(Resource):
 
     @jwt_required
     def delete(self, name):
-        claims = get_jwt_claims()
-        if not claims["is_admin"]:
-            return {"message": "Admin privilege required."}, 401
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -74,28 +67,16 @@ class Item(Resource):
                 item.save_to_db()
                 return item.json(), 201
             except:
-                return (
-                    {"message": "An error ocurred while inserting the item."},
-                    500,
-                )
+                return {"message": "An error ocurred while inserting the item."}, 500
         else:
             try:
                 item.price = data["price"]
                 item.save_to_db()
                 return item.json(), 200
             except:
-                return (
-                    {"message": "An error ocurred while updating the item."},
-                    500,
-                )
+                return {"message": "An error ocurred while updating the item."}, 500
 
 
 class ItemList(Resource):
-    @jwt_optional
     def get(self):
-        user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {"items": items}, 200
-        return {"items": [item["name"] for item in items],
-                "message": "More data available if you log in."}, 200
+        return {"items": [item.json() for item in ItemModel.find_all()]}, 200
