22490eb7162dafde87f2309566151f4ad96ea47d
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-24 16:36:44 -0300

    Add an activated property to UserModel

---
+++ adv-section3/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
+
+from resources.user import (
+    UserLogout,
+    UserRegister,
+    User,
+    UserLogin,
+    UserLogout,
+    TokenRefresh,
+)
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from ma import ma
+from blacklist import BLACKLIST
+from db import db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
+app.config["JWT_SECRET_KEY"] = "henrique"
+# app.secret_key = "henrique"
+api = Api(app)
+db.init_app(app)
+ma.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err): # except ValidationError as err
+    return jsonify(err.messages), 400
+
+jwt = JWTManager(app)
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token["jti"] in BLACKLIST
+
+@app.route("/")
+def index():
+    return """
+        <h1>Stores API with Flask &amp; FlaskRESTful</h1>
+        <hr>
+        <h3>Endpoints:</h3>
+        <ul>
+        <li>/items</li>
+        <li>/item/&lt;name&gt;</li>
+        <li>/auth</li>
+        <li>/register</li>
+        <li>/store/&lt;name&gt;</li>
+        <li>/stores</li>
+        </ul>
+        <hr>
+        """
+
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
