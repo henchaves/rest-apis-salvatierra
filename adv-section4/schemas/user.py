1d0cf76675d5a649fede6610119807fc3e8fbf51
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-27 19:48:21 -0300

    Create ConfirmationModel

---
+++ adv-section4/schemas/user.py
@@ -0,0 +1,9 @@
+from ma import ma
+from models.user import UserModel
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserModel
+        load_only = ("password", ) #Password will only be used on load (deserialize)
+        dump_only = ("id", "activated") #Id will only be used on dump (serialize)
+        load_instance = True
